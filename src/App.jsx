130a9829b0cea3defda9fb0e78c530e9253805af
Bekhan22 <shoislom149@gmail.com>
2025-07-17 16:05:58 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,348 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header class="container">
+        <img src="./pizzashop.png" alt="asds" />
+        <nav>
+          <ul>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Pricing</a></li>
+            <button class="btn">Log in</button>
+            <button className='btn2'><i class="fa-solid fa-bag-shopping"></i></button>
+          </ul>
+        </nav>
+        <button class="menu-btn"><i class="fa-solid fa-bars"></i></button>
+      </header>
+      <div className="texts container">
+        <div className="text">
+          <h1>The Fastest  <br />
+            Pizza Delivery</h1>
+          <p>
+            We will deliver juicy pizza for your family in 30 <br />
+            minutes, if the courier is late - pizza is free!
+          </p>
+          <p>Cooking process:</p>
+          <img src="./pizza.png" alt="ds" />
+
+          <div class="double-button">
+            <div class="left">To order</div>
+            <div class="right">Pizza–Menu</div>
+          </div>
+        </div>
+        <img className='imss' src="./sho.png" alt="dw" />
+      </div>
+      <h1 className=' container menu'>Menu</h1>
+      <div className="btns container">
+        <button className='btn'>Show All</button>
+        <p>Meat</p>
+        <p>Vegetarian</p>
+        <p>Sea products</p>
+        <p>Mushroom</p>
+      </div>
+      <div className=" container cards">
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+
+      </div>
+      <div className="container">
+        <img className='im' src="./ps.png" alt="" />
+        <h1 className='most'>MOST POPULAR PIZZA</h1>
+      </div>
+
+      <div className=" container cards">
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+        <div className="card">
+          <img src="./card.png" alt="" />
+          <h2>Italian</h2>
+          <p>Filling: onion, potato, tomato, mushrooms, cheese, olives, meat...</p>
+          <div className="bs">
+            <button className='btn3'>22</button>
+            <button className='btn4'>22</button>
+            <button className='btn3'>22</button>
+          </div>
+          <button className='btn5'> + Ingridients</button>
+          <div className="narx">
+            <h2>7,35 $</h2>
+            <button className='btn4'>-</button>
+            <p> 1 </p>
+            <button className='btn3'>+</button>
+          </div>
+          <button className='btn'>Order Now</button>
+        </div>
+
+
+      </div>
+      <div className="cards2 container">
+        <div className="event">
+          <div className="imgs1">
+            <img src="./p2.png" alt="" />
+            <div className="im1">
+              <h2>HOW  <br />
+                COOKING</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+
+          <div className="imgs1">
+            <img src="./p2.png" alt="" />
+            <div className="im1">
+              <h2>HOW  <br />
+                COOKING</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+          <div className="im2">
+            <h1>Events</h1>
+            <p>
+              There are regular events in our pizzeria that will <br />
+              allow you to eat delicious food for a lower price!
+            </p>
+          </div>
+        </div>
+        <div className="two">
+          <div className="imgs2">
+            <img src="t.png" alt="as" />
+            <div className="im3">
+              <h2>KITCHEN <br />
+                TOUR</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+
+          <div className="imgs2">
+            <img src="t.png" alt="as" />
+            <div className="im3">
+              <h2>KITCHEN <br />
+                TOUR</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+        </div>
+        <div className="event" id='t'>
+          <div className="imgs1">
+            <img src="./p2.png" alt="" />
+            <div className="im1">
+              <h2>HOW  <br />
+                COOKING</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+          <div className="imgs1">
+            <img src="./p2.png" alt="" />
+            <div className="im1">
+              <h2>HOW  <br />
+                COOKING</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+          <div className="imgs1">
+            <img src="./p2.png" alt="" />
+            <div className="im1">
+              <h2>HOW  <br />
+                COOKING</h2>
+              <button className='btn'>More</button>
+            </div>
+          </div>
+        </div>
+        <div className="texts2">
+          <div className="text2">
+            <h1>About us</h1>
+            <p>
+              In just a couple of years, we have opened 6 outlets <br />
+              in different cities: Kazan, Chelyabinsk, Ufa, Samara, <br />
+              Izhevsk, and in the future we plan to develop the <br />
+              network in other major cities of Russia.
+            </p>
+            <img src="./pizs.png" alt="ds" />
+            <p>
+              The kitchen of each point is at least: 400-500 <br />
+              sq. m. meters, hundreds of employees, smoothly <br />
+              performing work in order to receive / prepare / <br />
+              form / deliver customer orders on time.
+            </p>
+          </div>
+          <img className='top' src="./top.png" alt="ds" />
+        </div>
+      </div>
+      <footer className='container'>
+        <div className="ft">
+        <img src="./pizzashop.png" alt="" />
+
+        <div className="fh">
+          <h4>Home</h4>
+          <p>To Order</p>
+          <p>About us</p>
+          <p>Events</p>
+          <p>Menu</p>
+        </div>
+
+        <div className="fh">
+          <h4>Events</h4>
+          <p>3 Pizza 1 Free Coffee</p>
+          <p>2 Pizza for 1 Price</p>
+          <p>Kitchen Tour</p>
+        </div>
+
+        <div className="fh">
+          <h4>Menu</h4>
+          <p>Show All</p>
+          <p>Seaproducts</p>
+          <p>Vegan</p>
+          <p>Meat</p>
+        </div>
+
+        <div className="fh">
+          <h4>About Us</h4>
+          <p>Our History</p>
+          <p>Why We?</p>
+        </div>
+
+        </div>
+        <div className="tb">
+          <h2>+7 (937) 333-55-33</h2>
+
+          <div className="ins">
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-facebook"></i>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
